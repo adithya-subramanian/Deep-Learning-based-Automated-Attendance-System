33edf6e52d57e7834bf35eedd5c2fb5471beefc9
Unknown <anirudhsekar96@gmail.com>
2018-01-14 12:58:18 +0530

    removed an / before virtual env activation

--- Colfax.sh
+++ Colfax.sh
@@ -1,6 +1,6 @@
 #PBS -l nodes=10:knl
 
-source /FYP/bin/activate
+source FYP/bin/activate
 
 jupyter nbconvert --to notebook --execute ~/Deep-Learning-based-Automated-Attendance-System/WIDER_Dataset_Analysis.ipynb
 
