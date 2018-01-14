ed232ab8bbcde70d3a20299cf1a03a7632d5b1ac
u5372 <anirudhsekar96@gmail.com>
2018-01-14 02:37:26 -0800

    Proccesed the Dataset infomation for WIDER Dataset, and stored it in a convinient csv file

--- Colfax.sh
+++ Colfax.sh
@@ -2,6 +2,6 @@
 
 source FYP/bin/activate
 
-jupyter nbconvert --to notebook --execute ~/Deep-Learning-based-Automated-Attendance-System/WIDER_Dataset_Analysis.ipynb
+jupyter nbconvert --to html --ExecutePreprocessor.timeout=-1 --execute ~/Deep-Learning-based-Automated-Attendance-System/WIDER_Dataset_Analysis.ipynb
 
-deactivate
+source deactivate
