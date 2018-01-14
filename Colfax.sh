3f9e6ee56b901d0f7c9a346b89526b0bc04da655
Unknown <anirudhsekar96@gmail.com>
2018-01-14 12:49:12 +0530

    Updated the Colfax FIle

---
+++ Colfax.sh
@@ -0,0 +1,7 @@
+#PBS -l nodes=10:knl
+
+source /FYP/bin/activate
+
+jupyter nbconvert --to notebook --execute ~/Deep-Learning-based-Automated-Attendance-System/WIDER_Dataset_Analysis.ipynb
+
+deactivate
